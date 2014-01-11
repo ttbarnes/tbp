ce0a20cadc71be2e099000e4d1e5223d3b7e8253
ttbarnes <tony@tonybarnes.me>
2014-01-11 12:11:01 +0000

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var tbangApp = angular.module('tbpApp', [
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+	//'ngAnimate',
+	'tbangControllers'
+]);
+  
+	
+tbangApp.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/intro.html',
+			controller: 'introCtrl2'
+		})
+		.when('/showcase', {
+			templateUrl: 'views/showcase.html',
+			controller: 'showcaseGrid'
+		})
+		.when('/showcase/:phoneId', {
+			templateUrl: 'views/showcase-individual.html',
+			controller: 'indShowcasePieceCtrl'
+		});
+	//.otherwise({
+	//	redirectTo: '/'
+	//});
+});
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var tbpControllers = angular.module('tbangControllers', []);
+
+tbpControllers.controller('introCtrl2', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('showcase/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+  }]);
+	
+tbpControllers.controller('showcaseGrid', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('showcase/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+  }]);
+
+tbpControllers.controller('indShowcasePieceCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    //$scope.phoneId = $routeParams.phoneId;
+		
+    $http.get('showcase/' + $routeParams.phoneId + '.json').success(function(data) {
+      $scope.phone = data;
+    });
+		
+  }]);
