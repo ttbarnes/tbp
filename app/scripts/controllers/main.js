27b968cf94290d9708118b17907c638895c5f9a7
Tony Barnes <tony@tonybarnes.me>
2014-03-02 09:56:18 +0000

    revert about page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -62,18 +62,19 @@ tbpControllers.controller('indShowcasePieceCtrl', ['$scope', '$routeParams', '$h
 		
   }]);
 
-tbpControllers.controller('aboutCtrl', function($scope, CommonCode){
+tbpControllers.controller('aboutCtrl', function($scope){
 	/*
 	$scope.$on('$routeChangeSuccess', function() {
     window.scrollTo(0, 0); //fail safe
 	});
 	*/
 	
-
+/*
   $scope.alerter = CommonCode;
 	$scope.alerter.show('hello world! tb');
 	//$scope.tbthing500 = CommonCode;
 	//$scope.tbthing500.show();
+*/
 
 	$scope.pageClass = 'aboutPage';
 
