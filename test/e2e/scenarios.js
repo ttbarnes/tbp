8f10a5ac91254a19f74af3453331db3a625e79a6
Tony Barnes <tony@tonybarnes.me>
2014-03-09 17:03:53 +0000

    scenario test for contact view, renaming stuff

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,13 +1,11 @@
 'use strict';
 
-describe('Page: Intro', function() {
+describe('Page-view: Intro', function() {
 	beforeEach(function() {
     browser().navigateTo('/#/');
   });
 
-	//check page class has rendered as expected text
-
-	describe('recent releases', function() {
+	describe('Recent releases', function() {
 		it('should have 4 items assigned/limited', function(){
 			var x = '#recentReleases ul li';
 			expect(repeater(x).count()).toEqual(4);
@@ -18,7 +16,7 @@ describe('Page: Intro', function() {
 
 	});
 
-	describe('big buttons section', function() {
+	describe('Big buttons section', function() {
 		it('should have a button directing to the about page', function(){
 			var x = 'div.subIntro a.btn:first-child';
 		  element(x).click();
@@ -30,13 +28,51 @@ describe('Page: Intro', function() {
 			expect(browser().location().url()).toEqual('/showcase');
 		});
 	});
-  
-	
-
 
 });
 
-describe('Page: Error', function() { 
+
+/*
+describe('Footer', function() {
+	beforeEach(function() {
+    browser().navigateTo('/#/');
+  });
+
+	describe('Social icon links', function() {
+
+		it('should have 3 items', function(){
+
+		});
+
+	});
+
+});
+*/
+
+
+
+describe('Page-view: About', function(){
+	beforeEach(function() {
+    browser().navigateTo('/#/about');
+  });
+
+  //test social links
+
+});
+
+describe('Page-view: Contact', function(){
+	beforeEach(function() {
+    browser().navigateTo('/#/contact');
+  });
+
+	it('should have a correct email address', function() {
+		expect(element('a.contactAddress').html()).toEqual('tony@tonybarnes.me');
+	});
+
+});
+
+
+describe('Page-view: Error', function() {
 
 	it('should automatically redirect to /error/ when the window url is not recognised', function () {
     browser().navigateTo('/#/asdfqwertyzxcv1234');
