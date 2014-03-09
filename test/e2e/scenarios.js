3833e150eb7431a7e4dabe1c4b512db2a30828ea
Tony Barnes <tony@tonybarnes.me>
2014-03-09 12:07:15 +0000

    intro scenarios

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,5 +1,40 @@
 'use strict';
 
+describe('Page: Intro', function() {
+	beforeEach(function() {
+    browser().navigateTo('/#/');
+  });
+
+	//check page class has rendered as expected texa
+
+	//recent releases function start
+	//
+	//
+	it('should have 4 items', function(){
+		var x = '#recentReleases ul li';
+		expect(repeater(x).count()).toEqual(4);
+	});
+
+	//make sure categories/tags contain stuff
+
+	//
+	//
+	//recent releases function end
+
+  it('should have a button directing to the about page', function(){
+	  element('div.subIntro a.btn:first-child').click(); 
+		expect(browser().location().url()).toEqual('/about');
+	});
+
+	it('should have a button directing to the showcase page', function(){
+	  element('div.subIntro a.btn:last-child').click(); 
+		expect(browser().location().url()).toEqual('/showcase');
+	});
+	
+
+
+});
+
 describe('Page: Error', function() { 
 
 	it('should automatically redirect to /error/ when the window url is not recognised', function () {
@@ -18,3 +53,4 @@ describe('Page: Error', function() {
 	*/
 
 });
+
