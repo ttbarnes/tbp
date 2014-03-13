a1983e01a12c642cbe56a7708dc981770b99e4e0
ttbarnes <tony@tonybarnes.me>
2014-03-13 15:27:25 +0000

    fix grunt/karma unit testing issue
    e2e still not working

--- karma.conf.js
+++ karma.conf.js
@@ -12,10 +12,14 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-animate/angular-animate.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-resource/angular-resource.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
+      'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular/angular-mocks.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
-      'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
 
@@ -31,7 +35,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
