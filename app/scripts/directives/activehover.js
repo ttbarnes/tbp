b0dbea3c41609fd3e2f82ba142d7eceae0a31c9d
Tony Barnes <tony@tonybarnes.me>
2014-03-18 10:49:59 +0000

    custom directive for showcase gallery grid

--- app/scripts/directives/activehover.js
+++ app/scripts/directives/activehover.js
@@ -1,34 +1,24 @@
 'use strict';
 
 angular.module('tbpApp')
-  .directive('ngActiveHover', function () {
+  .directive('activeHover', function () {
     return {
-      link: function($scope, element){
-        console.log('activeHoverING');
-        /*
-				element.bind('mouseover', function(){
-					element.addClass('MOUSEDOVER');
-					//element.parent().addClass('SOMETHING-OR-RATHER');
-				});
-				element.bind('mouseout', function(){
-					element.removeClass('MOUSEDOVER');
-					//element.parent().removeClass('SOMETHING-OR-RATHER');
-				});
-				*/
-				$scope.$watch(element.children(),function(){
-			    //var children = element.children();
-			    //for(var i=0;i<children.length;i++){
-			    var children = element.children();
-			    for(var i=0;i<children.length;i++){
-		        if(children[i].nodeType !== 8){
-		          angular.element(children[i]).css('background', 'grey');
-		        }
-			    }
-				  element.bind('mouseover', function(){
-				    element.addClass('MOUSEDOVER');
-						//element.parent().addClass('SOMETHING-OR-RATHER');
+      link: function(scope, elem){
+
+				var allProjects = elem.parent().children('li');
+
+				scope.$watch(elem,function(){
+					elem.bind('mouseover', function(){
+						allProjects.addClass('inactive');
+						elem.addClass('active');
+						elem.nextAll('li').addClass('inactive'); //fail safe. must be a better way of doing this
 					});
-			    //}
+					elem.bind('mouseout', function(){
+						elem.removeClass('active');
+						allProjects.removeClass('inactive');
+						elem.nextAll('li').removeClass('inactive'); //fail safe. must be a better way of doing this
+					});
+
 				});
 
 			}
