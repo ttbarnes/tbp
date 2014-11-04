1e124ffd510d3339e32d6960b4ae39d877e82ce9
Tony Barnes <tony@tonybarnes.me>
2014-11-04 11:19:04 +0000

    tidy indentation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,9 +5,9 @@ var tbpControllers = angular.module('tbpControllers', []);
 tbpControllers.controller('introCtrl', ['$scope', '$http', 'windowScrollTop',
 
   function($scope, $http, windowScrollTop) {
-		$scope.pageClass = 'introPage';
-		
-		$http.get('showcase/recent-releases.json').success(function(data) {
+    $scope.pageClass = 'introPage';
+    
+    $http.get('showcase/recent-releases.json').success(function(data) {
       $scope.projects = data;
     });
 
@@ -16,55 +16,55 @@ tbpControllers.controller('introCtrl', ['$scope', '$http', 'windowScrollTop',
     };
 
   }]);
-	
+  
 tbpControllers.controller('showcaseGridCtrl', ['$scope', '$http', 'windowScrollTop',
-	function($scope, $http, windowScrollTop) {
-		
-		$scope.pageClass = 'showcaseHomePage';
-	
-		$http.get('showcase/all-projects.json').success(function(data) {
-			$scope.projects = data;
-		});
+  function($scope, $http, windowScrollTop) {
+    
+    $scope.pageClass = 'showcaseHomePage';
+  
+    $http.get('showcase/all-projects.json').success(function(data) {
+      $scope.projects = data;
+    });
 
-		//category filter
-		$scope.setSelectedCategory = function (value) {
-			$scope.selectedCategory = value;
-		};
-		$scope.categories = [
-		  'Ecommerce',
-			'Static',
-			'jQuery plugins',
-			'Holding pages',
-			'Brand identity',
-			'Designs, mockups'
-		];
-		
-		$scope.byCategory = function(project){
-			return project.category === $scope.selectedCategory;
-		};
+    //category filter
+    $scope.setSelectedCategory = function (value) {
+      $scope.selectedCategory = value;
+    };
+    $scope.categories = [
+      'Ecommerce',
+      'Static',
+      'jQuery plugins',
+      'Holding pages',
+      'Brand identity',
+      'Designs, mockups'
+    ];
+    
+    $scope.byCategory = function(project){
+      return project.category === $scope.selectedCategory;
+    };
 
-		$scope.selectedCategory = 'Ecommerce';
-		
-		/*
-		$scope.doSomething = function(){
-			console.log('hovered!');
-			angular.element('ul.showcaseGalleryGrid').addClass('SOMETHING-BOOM');
-		};
-		$scope.doSomethingELSE = function(){
-			angular.element('ul.showcaseGalleryGrid').removeClass('SOMETHING-BOOM');
-		};
-		*/
+    $scope.selectedCategory = 'Ecommerce';
+    
+    /*
+    $scope.doSomething = function(){
+      console.log('hovered!');
+      angular.element('ul.showcaseGalleryGrid').addClass('SOMETHING-BOOM');
+    };
+    $scope.doSomethingELSE = function(){
+      angular.element('ul.showcaseGalleryGrid').removeClass('SOMETHING-BOOM');
+    };
+    */
 
-		$scope.scrollTop = function () {
+    $scope.scrollTop = function () {
       windowScrollTop();
     };
 
-	}]);
+  }]);
 
 tbpControllers.controller('indShowcasePieceCtrl', ['$scope', '$routeParams', '$http', 'windowScrollTop',
   function($scope, $routeParams, $http, windowScrollTop) {
-		$scope.pageClass = 'showcaseIndPage';
-		
+    $scope.pageClass = 'showcaseIndPage';
+    
     $http.get('showcase/' + $routeParams.projectId+ '.json').success(function(data) {
       $scope.project = data;
     });
@@ -72,26 +72,26 @@ tbpControllers.controller('indShowcasePieceCtrl', ['$scope', '$routeParams', '$h
     $scope.scrollTop = function () {
       windowScrollTop();
     };
-		
+    
   }]);
 
 tbpControllers.controller('aboutCtrl', function($scope, windowScrollTop){
-	$scope.pageClass = 'aboutPage';
-	
-	$scope.scrollTop = function () {
+  $scope.pageClass = 'aboutPage';
+  
+  $scope.scrollTop = function () {
     windowScrollTop();
   };
 
 });
-	
+  
 tbpControllers.controller('contactCtrl', function($scope){
-	$scope.pageClass = 'contactPage';
-	$scope.tonyDetails = {
-		email:'tony@tonybarnes.me'
-	};
+  $scope.pageClass = 'contactPage';
+  $scope.tonyDetails = {
+    email:'tony@tonybarnes.me'
+  };
 });
 
 
 tbpControllers.controller('errorCtrl', function($scope){
-	$scope.pageClass = 'errorPage';
+  $scope.pageClass = 'errorPage';
 });
