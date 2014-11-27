5a3ec28353bf764de1fa980d6186d24f6d11b199
ttbarnes <tony@tonybarnes.me>
2014-11-27 20:42:41 +0000

    tidy up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,41 +11,41 @@ var tbp = angular.module('tbpApp', [
 ]);
 
 tbp.config(function ($routeProvider) {
-	$routeProvider
-		.when('/', {
-			templateUrl: 'views/intro.html',
-			controller: 'introCtrl'
-		})
-		.when('/showcase', {
-			templateUrl: 'views/showcase.html',
-			controller: 'showcaseGridCtrl'
-		})
-		.when('/showcase/jquery/active-hover-states', {
-			templateUrl: 'views/demos/ahs.html',
-		})
-		.when('/showcase/jquery/list-icon-manipulator', {
-			templateUrl: 'views/demos/lim.html',
-		})
-		.when('/showcase/jquery/portrolio', {
-			templateUrl: 'views/demos/portrolio.html',
-		})
-		.when('/showcase/:projectId', {
-			templateUrl: 'views/showcase-individual.html',
-			controller: 'indShowcasePieceCtrl'
-		})
-		.when('/about', {
-			templateUrl: 'views/about.html',
-			controller: 'aboutCtrl'
-		})
-		.when('/contact', {
-			templateUrl: 'views/contact.html',
-			controller: 'contactCtrl'
-		})
-		.otherwise({
-			redirectTo: '/error',
-			templateUrl: 'views/error.html',
-			controller: 'errorCtrl'
-		});
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/intro.html',
+      controller: 'introCtrl'
+    })
+    .when('/showcase', {
+      templateUrl: 'views/showcase.html',
+      controller: 'showcaseGridCtrl'
+    })
+    .when('/showcase/jquery/active-hover-states', {
+      templateUrl: 'views/demos/ahs.html',
+    })
+    .when('/showcase/jquery/list-icon-manipulator', {
+      templateUrl: 'views/demos/lim.html',
+    })
+    .when('/showcase/jquery/portrolio', {
+      templateUrl: 'views/demos/portrolio.html',
+    })
+    .when('/showcase/:projectId', {
+      templateUrl: 'views/showcase-individual.html',
+      controller: 'indShowcasePieceCtrl'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'aboutCtrl'
+    })
+    .when('/contact', {
+      templateUrl: 'views/contact.html',
+      controller: 'contactCtrl'
+    })
+    .otherwise({
+      redirectTo: '/error',
+      templateUrl: 'views/error.html',
+      controller: 'errorCtrl'
+    });
 })
 .run(function($rootScope, windowScrollTop) {
   $rootScope.$on('$routeChangeSuccess', function () {
