0ba9ebd894ae8d567802d4520ffcab90cb36c607
ttbarnes <tony@tonybarnes.me>
2016-10-19 12:52:06 +0100

    remove everything

--- app/scripts/services/windowScrollTop.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('tbpApp')
-  .service('windowScrollTop', function () {
-    return function() {
-      window.scrollTo(0,0);
-      //would be good to have an optional transition fade (for the on click)
-    };
-  });
