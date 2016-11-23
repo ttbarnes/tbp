956f37b4cea12d1a859d49fedfb000f46a525543
ttbarnes <tony@tonybarnes.me>
2016-11-23 16:07:06 +0000

    music page items prep

--- app/containers/MusicPage/index.js
+++ app/containers/MusicPage/index.js
@@ -8,6 +8,27 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import MusicPlayer from 'components/MusicPlayer';
 
+const tempMusicData = [
+  {
+    id: 1,
+    title: 'test title',
+    url : 'http://soundcloud.com/ttonyb/asdf',
+    genre: 'House'
+  },
+  {
+    id: 2,
+    title: 'dummy title',
+    url : 'http://soundcloud.com/ttonyb/qwerty',
+    genre: 'House'
+  },
+  {
+    id: 3,
+    title: 'dummy title',
+    url : 'http://soundcloud.com/ttonyb/qwerty',
+    genre: 'Downtempo'
+  }
+];
+
 export class MusicPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
   render() {
@@ -24,7 +45,17 @@ export class MusicPage extends React.PureComponent { // eslint-disable-line reac
 
         <p>MusicPage</p>
 
-        <MusicPlayer/>
+        {
+          tempMusicData.map((t) => {
+            return (
+              <li key={t.id}>
+                {t.title}
+                <MusicPlayer />
+              </li>
+            )
+          })
+        }
+
 
       </article>
     );
