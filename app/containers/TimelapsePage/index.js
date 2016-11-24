e57873ebda323dd9e8936ffeeb970cee2bdad07a
ttbarnes <tony@tonybarnes.me>
2016-11-24 16:25:48 +0000

    timelapse video playing

--- app/containers/TimelapsePage/index.js
+++ app/containers/TimelapsePage/index.js
@@ -6,6 +6,31 @@
 
 import React from 'react';
 import Helmet from 'react-helmet';
+import YouTube from 'react-youtube';
+
+const data = [
+  {
+    title: 'Cycling in Essex',
+    videoId: 'VHusMdGaOoU',
+    description: 'Voluptas sit aspernatur aut odit aut fugit'
+  },
+  {
+    title: 'Night lapse stars test',
+    videoId: 'HVvcGT2bWEo',
+    description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit'
+  },
+  {
+    title: 'Canal boat time lapse (2 hours) - Aldermaston to Devizes Summer 2016',
+    videoId: 'fKgHG4c1iA8',
+    description: 'Aspernatur aut odit aut fugit'
+  }
+];
+
+const youTubeOpts = {
+  playerVars: {
+    showinfo: 0
+  }
+};
 
 export class TimelapsePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
@@ -21,7 +46,15 @@ export class TimelapsePage extends React.PureComponent { // eslint-disable-line 
           ]}
         />
 
-        <p>TimelapsePage</p>
+        {
+          data.map(t => (
+            <li key={t.videoId}>
+              <h4>{t.title}</h4>
+              <p>{t.description}</p>
+              <YouTube videoId={t.videoId} opts={youTubeOpts} />
+            </li>
+          ))
+        }
 
       </article>
     );
