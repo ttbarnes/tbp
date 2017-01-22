0c2726ccb6d6da66088584e2fe71f5e70f4f719e
ttbarnes <tony@tonybarnes.me>
2017-01-22 12:53:03 +0000

    remove grunt bower task

--- Gruntfile.js
+++ Gruntfile.js
@@ -358,7 +358,7 @@ module.exports = function (grunt) {
 
     grunt.task.run([
       'clean:server',
-      'bower-install',
+      // 'bower-install',
       'concurrent:server',
       //'autoprefixer',
       'connect:livereload',
