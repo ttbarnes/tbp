f2141ada1285e17388ec8f09d89f24b59dc1b871
ttbarnes <tony@tonybarnes.me>
2017-02-26 22:04:51 +0000

    fixed nav poc

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -2,29 +2,31 @@ import React, { PropTypes } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import NavBar from './NavBar';
-import HeaderLink from './HeaderLink';
 import messages from './messages';
+import { Root, StyledHeaderLink } from './styled';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     const { location } = this.props;
     const isHomePage = location.pathname === '/';
 
+    if (isHomePage) {
+      return null;
+    }
+
     return (
-      <div>
-        {!isHomePage && (
-          <NavBar>
-            <HeaderLink to="/">
-              <FormattedMessage {...messages.home} />
-            </HeaderLink>
-            <HeaderLink to="/dev">dev</HeaderLink>
-            <HeaderLink to="/music">music</HeaderLink>
-            <HeaderLink to="/fpv">fpv</HeaderLink>
-            <HeaderLink to="/timelapse">timelapse</HeaderLink>
-            <HeaderLink to="/contact">contact</HeaderLink>
-          </NavBar>
-        )}
-      </div>
+      <Root>
+        <NavBar>
+          <StyledHeaderLink to="/">
+            <FormattedMessage {...messages.home} />
+          </StyledHeaderLink>
+          <StyledHeaderLink to="/dev">dev</StyledHeaderLink>
+          <StyledHeaderLink to="/music">music</StyledHeaderLink>
+          <StyledHeaderLink to="/fpv">fpv</StyledHeaderLink>
+          <StyledHeaderLink to="/timelapse">timelapse</StyledHeaderLink>
+          <StyledHeaderLink to="/contact">contact</StyledHeaderLink>
+        </NavBar>
+      </Root>
     );
   }
 }
---
+++ app/components/Header/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import styles from './styles';
+import HeaderLink from './HeaderLink';
+
+export const Root = styled.div`${styles.root}`;
+
+export const StyledHeaderLink = styled(HeaderLink)`${styles.link}`;
