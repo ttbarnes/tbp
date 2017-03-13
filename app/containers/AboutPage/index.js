bf837a6f377731f79c204d430662c253abc08aea
Tony Barnes <tony@tonybarnes.me>
2017-03-13 13:10:38 +0000

    update about copy

--- app/containers/AboutPage/index.js
+++ app/containers/AboutPage/index.js
@@ -18,9 +18,11 @@ export class AboutPage extends React.PureComponent { // eslint-disable-line reac
 
         <H1>About</H1>
         <p>I am a passionate front end {'/'} javascript {'/'} angular {'/'} full stack JS developer with many ambitions.</p>
-        <p>I have a lot of experience with many market sectors such as ecommerce, health, fin-tech, book publishing, mortgage comparison and many more. I have led and grown front end teams - managing both business and development requirements whilst ensuring that clean, scalable solutions are delivered.</p>
+        <p>
+I have a lot of experience with many market sectors such as ecommerce, health, travel, fin-tech, book publishing, mortgage comparison and more. I have led and grown front end teams and always ensure that clean, scalable solutions are delivered.</p>
         <p>I really enjoy collaborating and open source projects. I{'\''}ve built or contributed to 17 angular apps in 2 years – 3 including full stack JS. I{'\''}ve worked closely with many developers in various environments utilising agile/scrum methodologies. Thriving on the latest technologies, I am constantly improving my skill set. I{'\''}m currently building a new startup idea utilising the MEAN stack. Previously built a {'\''}mini twitter{'\''}. My current venture is React.</p>
         <p>Download <Link to={CV} target="_blank">CV</Link>( ? )</p>
+        
       </article>
     );
   }
