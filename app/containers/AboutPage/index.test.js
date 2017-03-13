095779ecfeec3a17e014abd7ca5680e2f66138f4
ttbarnes <tony@tonybarnes.me>
2017-03-13 22:10:56 +0000

    about unit tests

--- app/containers/AboutPage/index.test.js
+++ app/containers/AboutPage/index.test.js
@@ -1,25 +1,22 @@
-/*
 import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
 import Helmet from 'react-helmet';
-import { About } from '../index';
-
+import { AboutPage } from './index';
 
 describe('<About />', () => {
   const wrapper = shallow(
-    <About />
+    <AboutPage />
   );
   it('should have helmet component', () => {
     const actual = wrapper.containsMatchingElement(
       <Helmet
         title="About"
         meta={[
-          { name: 'description', content: 'About Tony Barnes' },
+          { name: 'description', content: 'About Tony Barnes' }
         ]}
       />
     );
     expect(actual).toEqual(true);
   });
 });
-*/
