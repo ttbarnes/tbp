2d9c18a6a1da034f97712108805b652ab78a67be
ttbarnes <tony@tonybarnes.me>
2017-03-17 08:57:58 +0000

    about page styling

--- app/containers/AboutPage/index.js
+++ app/containers/AboutPage/index.js
@@ -3,7 +3,6 @@ import Helmet from 'react-helmet';
 // import { Link } from 'react-router';
 import H1 from '../../components/H1';
 import List from '../../components/List';
-import Tag from '../../components/Tag';
 // import CV from '../../assets/Tony-Barnes-CV-2017.pdf';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -11,6 +10,11 @@ import {
   selectAboutTech
 } from './selectors';
 // import { FadeIn } from './styled';
+import {
+  StyledSection,
+  StyledList,
+  StyledTag
+} from './styled';
 
 export const techGroup = (arr, group) =>
   arr.filter((t) => {
@@ -24,7 +28,7 @@ export const renderTechGroup = (arr, groupType) => {
   const group = this.techGroup(arr, groupType);
   if (group.length) {
     return group[0].tags.map((i) =>
-      <Tag type={i} key={i} />
+      <StyledTag type={i} key={i} />
     );
   }
   return null;
@@ -47,40 +51,62 @@ export class AboutPage extends React.PureComponent { // eslint-disable-line reac
         />
 
         <H1>About</H1>
-        <p>I am a passionate front end {'/'} javascript {'/'} angular {'/'} full stack JS developer with many ambitions.</p>
-        <p>I have a lot of experience with many market sectors such as ecommerce, health, travel, fin-tech, book publishing, mortgage comparison and more. I have led and grown front end teams and always ensure that clean, scalable solutions are delivered.</p>
-        <p>I really enjoy collaborating and open source projects. I{'\''}ve built or contributed to 17 angular apps in 2 years – 3 including full stack JS. I{'\''}ve worked closely with many developers in various environments utilising agile/scrum methodologies. Thriving on the latest technologies, I am constantly improving my skill set. I{'\''}m currently building a new startup idea utilising the MEAN stack. Previously built a {'\''}mini twitter{'\''}. My current venture is React.</p>
-        {/* <p>Download <Link to={CV} target="_blank">CV</Link>( ? )</p> */}
+        <StyledSection>
+          <p>todo: add X years experience</p>
+          <p>I am a passionate front end {'/'} javascript {'/'} angular {'/'} full stack JS developer with many ambitions.</p>
+          <p>Striving for clean and scalable solutions, I thrive on the latest technologies. I am constantly improving my skill set. I{'\''}m currently building a new startup idea utilising the MEAN stack. Also creating a mini CMS with React. Previously built a {'\''}mini twitter{'\''}.</p>
+          <p>I really enjoy collaborating. I{'\''}ve built or contributed to 17 angular apps in 2 years – 3 including full stack JS.</p>
+          <p>I{'\''}ve worked closely with many developers in various environments, of course with agile/scrum {':)'}</p>
+        </StyledSection>
 
-        <p>Some tech i{'\''}ve been using lately:</p>
-        <List>
-          {renderTechGroup(tech, 'lately')}
-        </List>
+        <StyledSection>
+          <p>I have a lot of experience with many market sectors such as:</p>
+          <List showListStyle>
+            <p>Ecommerce</p>
+            <p>Health</p>
+            <p>Travel</p>
+            <p>Fin-tech</p>
+            <p>Book Publishing</p>
+            <p>Mortgage comparison</p>
+            <p>many more</p>
+          </List>
+          {/* <p>Download <Link to={CV} target="_blank">CV</Link>( ? )</p> */}
+        </StyledSection>
 
-        <br />
-        <br />
+        <StyledSection>
+          <p>Some tech i{'\''}ve been using lately:</p>
+          <StyledList>
+            {renderTechGroup(tech, 'lately')}
+          </StyledList>
+        </StyledSection>
 
-        <p>I usually write tests with:</p>
+        <StyledSection>
+          <p>I usually write tests with:</p>
 
-        <List>
-          {renderTechGroup(tech, 'testing')}
-        </List>
+          <StyledList>
+            {renderTechGroup(tech, 'testing')}
+          </StyledList>
+        </StyledSection>
 
-        <br />
-        <br />
-        <p>..and I can style with all the syntactically similar things:</p>
+        <StyledSection>
+          <p>..and I style with all the similar things:</p>
 
-        <List>
-          {renderTechGroup(tech, 'styling')}
-        </List>
+          <StyledList>
+            {renderTechGroup(tech, 'styling')}
+          </StyledList>
+        </StyledSection>
 
-        <p>Some other things I know</p>
+        <StyledSection>
+          <p>Some other things I know:</p>
 
-        <List>
-          {renderTechGroup(tech, 'other')}
-        </List>
+          <StyledList>
+            {renderTechGroup(tech, 'other')}
+          </StyledList>
+        </StyledSection>
 
-        <p>I used to use jQuery, Grunt and SVN, but I won{'\''}t go into details.</p>
+        <StyledSection>
+          <p>Previously I used jQuery, Grunt and SVN...</p>
+        </StyledSection>
 
       </article>
     );
--- app/containers/AboutPage/styled.js
+++ app/containers/AboutPage/styled.js
@@ -1,4 +1,6 @@
 import styled, { keyframes } from 'styled-components';
+import List from 'components/List';
+import Tag from 'components/Tag';
 
 const fadeIn = keyframes`
   from {
@@ -13,4 +15,18 @@ const fadeIn = keyframes`
 export const FadeIn = styled.div`
   display: inline-block;
   animation: ${fadeIn} 1s linear;
+`;
+
+export const StyledSection = styled.section`
+  margin-bottom: 5em;
+`;
+
+export const StyledList = styled(List)`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export const StyledTag = styled(Tag)`
+  margin-bottom: 0;
 `;
