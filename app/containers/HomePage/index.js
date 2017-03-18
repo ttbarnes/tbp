34bcc477bee22b356867c8f81d4bd472646f615c
ttbarnes <tony@tonybarnes.me>
2017-03-18 16:54:38 +0000

    home page styling

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router';
-import H1 from 'components/H1';
 import {
+  Root,
   FlexRoot,
-  StyledImg
+  Flex,
+  StyledH1,
+  StyledImg,
+  Intro
 } from './styled';
 import TbImage from '../../assets/img/tony-barnes.jpg';
 
@@ -12,7 +15,7 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
 
   render() {
     return (
-      <article>
+      <Root>
         <Helmet
           title="Front end developer, techy, musician"
           meta={[
@@ -20,17 +23,22 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
           ]}
         />
         <FlexRoot>
-          <div>
+          <Flex>
             <StyledImg src={TbImage} alt="Tony Barnes" />
-          </div>
+          </Flex>
           <div>
-            <H1>Hello! I{'\''}m Tony.</H1>
+            <StyledH1>Hello! I{'\''}m Tony.</StyledH1>
             <p>Developer, musician, cyclist, FPV pilot, tea drinker.</p>
-            <br />
-            <p>Read <Link to="about">about me</Link>, <br />check out <Link to="projects">my projects</Link>, <br />or see <Link to="hobbies">what else I get up to.</Link></p>
           </div>
         </FlexRoot>
-      </article>
+
+        <FlexRoot>
+          <Flex></Flex>
+          <Intro>
+            <p>Read <Link to="about">about me</Link>, <br />check out <Link to="projects">my projects</Link>, <br />or see <Link to="hobbies">what else I get up to.</Link></p>
+          </Intro>
+        </FlexRoot>
+      </Root>
     );
   }
 }
--- app/containers/HomePage/styled.js
+++ app/containers/HomePage/styled.js
@@ -1,19 +1,41 @@
 import styled from 'styled-components';
 import Img from 'components/Img';
+import H1 from 'components/H1';
 import { media } from 'components/styledShared';
+
+export const Root = styled.article`
+  max-width: 550px;
+  width: 100%;
+  margin: 0 auto;
+`;
 
 export const FlexRoot = styled.article`
   text-align: center;
-  ${media.xs`
+  align-items: center;
+  ${media.sm`
+    display: flex;
     text-align: left;
   `}
+`;
+
+export const Flex = styled.div`
+  flex: 0 0 160px;
   ${media.md`
-    display: flex;
+    flex: 0 0 175px;
   `}
+`;
+
+export const StyledH1 = styled(H1)`
+  margin: 0;
 `;
 
 export const StyledImg = styled(Img)`
   max-width: 150px;
   border-radius: 50%;
   padding-right: 1em;
+  margin-bottom: 0.5em;
+`;
+
+export const Intro = styled.div`
+  padding-top: 0.5em;
 `;
