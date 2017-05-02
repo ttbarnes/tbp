7b62aea4f34049b01d1a2949eebf19547089f70b
ttbarnes <tony@tonybarnes.me>
2017-05-02 14:53:38 +0100

    delete angular app

--- client.app.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-
-  // frontend routes =========================================================
-  // route to handle all angular requests
-  app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/dist/index.html');
-  });
-
-};
