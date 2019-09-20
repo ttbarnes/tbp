6f257f1daec1e806c63f805bd9396afaaa6307b9
ttbarnes <tony@tonybarnes.me>
2019-09-20 08:37:21 +0100

    remove old page

--- app/reducers.js
+++ app/reducers.js
@@ -1,25 +1,14 @@
-/**
- * Combine all reducers in this file and export the combined reducers.
- */
-
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import clientsReducers from 'containers/ClientsPage/reducer';
-// import hobbiesReducers from 'containers/HobbiesPage/reducer';
 import servicesReducer from 'reducers/services';
 import techReducers from 'containers/TechnologiesPage/reducer';
 
-/**
- * Creates the main reducer with the asynchronously loaded ones
- */
-// export default function createReducer(injectedReducers) {
 const createRootReducer = (history) => combineReducers({
   router: connectRouter(history),
   clients: clientsReducers,
   services: servicesReducer,
-  // hobbies: hobbiesReducers,
   tech: techReducers
-  // ...injectedReducers
 });
 
 export default createRootReducer;
