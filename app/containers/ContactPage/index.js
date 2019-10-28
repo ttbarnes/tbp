ec24730682c6e22047d5c8245e66f2062a672c1f
ttbarnes <tony@tonybarnes.me>
2019-10-28 12:15:03 +0000

    chat widget tweaks

--- app/containers/ContactPage/index.js
+++ app/containers/ContactPage/index.js
@@ -6,8 +6,43 @@ import H1 from '../../components/H1';
 import H2 from '../../components/H2';
 import { FadeIn } from '../../components/styledShared';
 
-export class ContactPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export class ContactPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderLiveChat: false
+    };
+    this.handleOnChatLoaded = this.handleOnChatLoaded.bind(this);
+  }
+
+  componentWillUnmount() {
+    this.setState({
+      renderLiveChat: false
+    });
+  }
+
+  onClickLiveChat() {
+    window.LC_API.open_chat_window();
+  }
+
+  handleOnChatLoaded() {
+    const renderLiveChat = (window.LC_API &&
+                            window.LC_API.open_chat_window &&
+                            typeof window.LC_API.open_chat_window === 'function');
+    if (renderLiveChat) {
+      this.setState({
+        renderLiveChat: true
+      });
+    } else {
+      this.setState({
+        renderLiveChat: false
+      });
+    }
+  }
+
   render() {
+    const { renderLiveChat } = this.state;
+
     return (
       <PageContainer smallWidth>
 
@@ -21,12 +56,23 @@ export class ContactPage extends React.PureComponent { // eslint-disable-line re
         <H1 hide>Contact</H1>
         <H2 h1Size>I{'\''}d love to talk, i{'\''}m very friendly!</H2>
         <FadeIn>
-          <p><a href="mailto:tony@tonybarnes.me">tony{'@'}tonybarnes.me</a></p>
-          <p><a href="http://twitter.com/ttbarnes" target="_blank" rel="noopener">{'@'}ttbarnes</a></p>
+          <p><a href="mailto:tony@tonybarnes.me">tony@tonybarnes.me</a></p>
+          <p><a href="http://twitter.com/ttbarnes" target="_blank" rel="noopener">@ttbarnes</a></p>
+          {renderLiveChat && (
+            <p>
+              <button
+                type="button"
+                onClick={this.onClickLiveChat}
+              >Chat now!
+              </button>
+            </p>
+          )}
         </FadeIn>
 
-        <button onClick={() => window.LC_API.open_chat_window()}>Chat now!</button>
-        <LiveChat license={10738687} />
+        <LiveChat
+          license={11437078}
+          onChatLoaded={this.handleOnChatLoaded}
+        />
 
       </PageContainer>
     );
