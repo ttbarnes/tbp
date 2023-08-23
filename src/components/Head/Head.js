e2a3ee2d91f76f5822635cf03a4f078d608f3fc6
ttbarnes <tony@tonybarnes.me>
2023-08-23 19:57:47 +0100

    feat(social): meta tags/image

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import NextHead from 'next/head';
+import { URL } from '@/constants';
 import { METADATA, NAME } from '@/content-strings';
 
 const {
@@ -19,11 +20,29 @@ const Head = ({ pageTitle }) => {
         name='viewport'
         content='height=device-height,width=device-width,initial-scale=1.0'
       />
+
       <title>{title}</title>
 
       <meta name='description' content={DESCRIPTION} />
       <meta name='author' content={NAME} />
       <meta name='keywords' content={KEYWORDS} />
+
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={DESCRIPTION} />
+      <meta property='og:url' content={URL} />
+      <meta
+        property='og:image'
+        content='/og-image.jpg'
+      />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta property='twitter:domain' content={URL} />
+      <meta property='twitter:url' content={URL} />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={DESCRIPTION} />
+      <meta name='twitter:image' content='/og-image.jpg' />
+
     
       <link rel='icon' href='/favicon.ico' />
     </NextHead>
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,4 @@ export * from './external-links';
 export * from './routes';
 
 export const EMAIL = 'tony@tonybarnes.me';
+export const URL = 'https://tonybarnes.me';
