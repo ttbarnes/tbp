3b5d31dec4589d7f047610efbc31a30b14327a53
ttbarnes <tony@tonybarnes.me>
2023-09-14 18:48:24 +0100

    feat(sidebar): active item link style

--- src/components/Sidebar/SidebarListItem/SidebarListItem.js
+++ src/components/Sidebar/SidebarListItem/SidebarListItem.js
@@ -1,21 +1,34 @@
+import { usePathname } from 'next/navigation';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import styles from './SidebarListItem.module.scss';
 
-const SidebarListItem = ({ title, href, onClick }) => (
-  <li
-    key={title}
-    className={styles.listItem}
-  >
-    <Link
-      href={href}
-      className={styles.listItemLink}
-      onClick={onClick}
+const SidebarListItem = ({ title, href, onClick }) => {
+  const pathname = usePathname();
+
+  const isActive = pathname === href;
+
+  let className = styles.listItemLink;
+
+  if (isActive) {
+    className += ` ${styles.listItemLinkActive}`;
+  }
+
+  return (
+    <li
+      key={title}
+      className={styles.listItem}
     >
-      <small>{title}</small>
-    </Link>
-  </li>
-);
+      <Link
+        href={href}
+        className={className}
+        onClick={onClick}
+      >
+        <small>{title}</small>
+      </Link>
+    </li>
+  );
+};
 
 SidebarListItem.propTypes = {
   title: PropTypes.string.isRequired,
