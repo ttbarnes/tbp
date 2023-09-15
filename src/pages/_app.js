92642f30bf48b00f68aa5803aa191a7e50c383c8
ttbarnes <tony@tonybarnes.me>
2023-09-15 01:03:06 +0100

    chore(tech) easter egg

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 import Metadata from '@/components/Head';
 import '@/styles/globals.scss';
-import { REPO } from '../constants';
+import { FRED, FRED_EASTER_EGG, REPO } from '../constants';
 
 const isProdEnv = process.env.NODE_ENV === 'production';
 
@@ -17,8 +18,18 @@ const { hello, seeTheCode, repoLink, bonusPoints } = easterEggMessages;
 const easterEggMessage = `${hello} ${seeTheCode} \n${repoLink} \n${bonusPoints}`;
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  const queryParams = router.query;
+
   if (isProdEnv) {
     console.info(easterEggMessage);
+  }
+
+  if (queryParams) {
+    if (Object.keys(queryParams).includes(FRED)) {
+      console.info(FRED_EASTER_EGG);
+    }
   }
 
   return (
