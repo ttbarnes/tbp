869b5acd9f15892408e6b84c8a5fdc533c98297a
ttbarnes <tony@tonybarnes.me>
2023-09-19 17:56:03 +0100

    chore(tech): remove easter egg

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,8 +7,3 @@ export const EMAIL = 'tony@tonybarnes.me';
 export const EMAIL_SUBJECT = `Hello from ${URL}`;
 
 export const REPO = 'https://github.com/ttbarnes/tbp';
-
-export const FRED = 'fred';
-
-export const FRED_EASTER_EGG =
-  'PYLON - Standby - Objects behind are losing. Downhill Rabbit Hill. HP4H  🇨🇦';
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types';
-import { useRouter } from 'next/router';
 import Metadata from '@/components/Head';
 import '@/styles/globals.scss';
-import { FRED, FRED_EASTER_EGG, REPO } from '../constants';
+import { REPO } from '../constants';
 
 const isProdEnv = process.env.NODE_ENV === 'production';
 
@@ -18,18 +17,8 @@ const { hello, seeTheCode, repoLink, bonusPoints } = easterEggMessages;
 const easterEggMessage = `${hello} ${seeTheCode} \n${repoLink} \n${bonusPoints}`;
 
 const App = ({ Component, pageProps }) => {
-  const router = useRouter();
-
-  const queryParams = router.query;
-
   if (isProdEnv) {
     console.info(easterEggMessage);
-  }
-
-  if (queryParams) {
-    if (Object.keys(queryParams).includes(FRED)) {
-      console.info(FRED_EASTER_EGG);
-    }
   }
 
   return (
