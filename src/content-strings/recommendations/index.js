7fc421c696b2249c2213a7fdb1284d55ce29ccc3
ttbarnes <tony@tonybarnes.me>
2023-09-29 20:31:08 +0100

    feat(recommendations): copy updates

--- src/content-strings/recommendations/index.js
+++ src/content-strings/recommendations/index.js
@@ -1,28 +1,6 @@
 export const RECOMMENDATIONS = [
   {
     authorText: [
-      'I have had the pleasure of working with Tony for the past 2 years working in the same team. He is one of the best among all people I have ever worked with.',
-      'Tony Barnes is an achievement oriented, multi-skilled, open minded and motivated web developer with a very strong work ethic. He has an extensive knowledge of modern development techniques, and his open minded approach could not be overestimated. I strongly recommend working with Tony.'
-    ],
-    author: 'Armand Rozanski',
-    role: 'Software Development Engineer at Warner Bros. Discovery'
-  },
-  {
-    authorText: [
-      'I really appreciated working with Tony at UK Export Finance. Dedicated, diligent and a clear sense of quality and standards. He was an absolutely key part of delivery especially when the project was under intense pressure and timescales. A great human and a great colleague. Thank you!'
-    ],
-    author: 'David Carboni',
-    role: 'CTO, Policy in Practice'
-  },
-  {
-    authorText: [
-      'I had the pleasure of working with Tony on numerous eCommerce projects during his time at Powa. He is a very skilled developer and problem solver and also managed his team very well when it came to allocating resources. He is very flexible with his approach and always supported our project management team well with project delivery.'
-    ],
-    author: 'Tamique Weekes',
-    role: 'Product @ Meta'
-  },
-  {
-    authorText: [
       "I've had the pleasure of working closely with Tony for several years on both government and startup projects, and I wholeheartedly recommend him. Tony is an exceptional full-stack developer who consistently turns design concepts into fully functional apps.",
       "His attention to detail and dedication to excellence make him a valuable asset to any team. Tony's technical skills and collaborative nature truly set him apart. If you're looking for a top-tier full-stack developer, Tony is your guy."
     ],
@@ -31,10 +9,10 @@ export const RECOMMENDATIONS = [
   },
   {
     authorText: [
-      "In addition to being an excellent developer, Tony also has the strategic vision to deliver as a lead developer. I thoroughly enjoyed working with Tony and didn't hesitate to bring him into important client situations. Would work with him again without question."
+      'I really appreciated working with Tony at UK Export Finance. Dedicated, diligent and a clear sense of quality and standards. He was an absolutely key part of delivery especially when the project was under intense pressure and timescales. A great human and a great colleague. Thank you!'
     ],
-    author: 'Steve Emecz',
-    role: 'VP Partnerships at Tungsten Network'
+    author: 'David Carboni',
+    role: 'CTO, Policy in Practice'
   },
   {
     authorText: [
@@ -46,7 +24,29 @@ export const RECOMMENDATIONS = [
   },
   {
     authorText: [
-      "I really enjoyed working on Tony's front-end team at Powa. He is an exceptionally driven and self-motivated developer, always exploring the latest technologies. He has a knack for remaining unbeatably friendly and approachable even when under pressure and will always take the time to listen to his teammates and offer help and advice."
+      'I have had the pleasure of working with Tony for the past 2 years working in the same team. He is one of the best among all people I have ever worked with.',
+      'Tony is an achievement oriented, multi-skilled, open minded and motivated developer with a very strong work ethic. He has an extensive knowledge of modern development techniques, and his open minded approach could not be overestimated. I strongly recommend working with Tony.'
+    ],
+    author: 'Armand Rozanski',
+    role: 'Software Development Engineer at Warner Bros. Discovery'
+  },
+  {
+    authorText: [
+      "In addition to being an excellent developer, Tony also has the strategic vision to deliver as a lead developer. I thoroughly enjoyed working with Tony and didn't hesitate to bring him into important client situations. Would work with him again without question."
+    ],
+    author: 'Steve Emecz',
+    role: 'VP Partnerships at Tungsten Network'
+  },
+  {
+    authorText: [
+      'I had the pleasure of working with Tony on numerous eCommerce projects during his time at Powa. He is a very skilled developer and problem solver and also managed his team very well when it came to allocating resources. He is very flexible with his approach and always supported our project management team well with project delivery.'
+    ],
+    author: 'Tamique Weekes',
+    role: 'Product @ Meta'
+  },
+  {
+    authorText: [
+      "I really enjoyed working on Tony's team. He is an exceptionally driven and self-motivated developer, always exploring the latest technologies. He has a knack for remaining unbeatably friendly and approachable even when under pressure and will always take the time to listen to his teammates and offer help and advice."
     ],
     author: 'Marek Muchlinski',
     role: 'Front-end developer'
